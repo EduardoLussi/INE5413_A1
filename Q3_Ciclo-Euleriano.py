c3c0bf855e2ab7753c422e84d0e3ec1968214e5d
EduardoLussi <eduardolussi@gmail.com>
2021-07-01 12:11:26 -0300

    Q3 - Ciclo Euleriano

--- Q3_Ciclo-Euleriano.py
+++ Q3_Ciclo-Euleriano.py
@@ -2,62 +2,70 @@ from Q1_Grafo import Grafo
 
 
 def buscarSubcicloEuleriano(grafo, v, C):
-    text = f"Função auxiliar: v={v.rotulo}, C={[c.rotulo for c in C]}"
-    print(text)
-
-    Ciclo = [v]
-    t = v
+    Ciclo = [v] # Inicia ciclo com o primeiro vértice
+    t = v   # Salva primeiro vértice
 
     while True:
-        print(f"Vizinhos de {v.rotulo}: {[viz.rotulo for viz in grafo.vizinhos(v)]}")
-        if len(C) == len(grafo.arestasVizinhos(v)): # Pode ter algo aqui
-            print("Não existe aresta não-visitada conectada a Ciclo")
+        # Só prossegue se existir uma aresta não-visitada conectada a Ciclo
+        # Transforma lista de arestas de v e visitadas C em um conjuntos e verifica se
+        # as arestas de v estão contidas em C
+        if set(grafo.arestasVizinhos(v)).issubset(set(C)):
             return False, None
 
+        # Seleciona uma aresta não visitada
         for aresta in grafo.arestasVizinhos(v):
             if aresta not in C:
-                # Redefine v
+                # Redefine v -> vértice atual
                 if v == aresta.vertices[0]:
                     v = aresta.vertices[1]
                 else:
                     v = aresta.vertices[0]
 
-                C.append(aresta)
+                C.append(aresta)    # adiciona aresta a visitadas
 
                 break
 
-        Ciclo.append(v)
-        print(f"Ciclo: {[ver.rotulo for ver in Ciclo]}")
+        Ciclo.append(v) # Adiciona vértice ao ciclo
 
-        if v == t:
+        if v == t:  # Finaliza quando fechar o ciclo
             break
 
-    naoVistadas = []
-    for x in Ciclo:
-        for aresta in grafo.arestasVizinhos(x):
-            if aresta not in C:
-                naoVistadas.append(aresta)
+    # Verifica se há subciclos enquanto houver vértices que possuem arestas não visitadas
+    while True:
+        naoVisitado = -1
+        for x in Ciclo: # Encontra vértice com aresta não visitada
+            for aresta in grafo.arestasVizinhos(x):
+                if aresta not in C:
+                    naoVisitado = x
+                    break
+            if naoVisitado != -1:
+                break
 
-    for x in naoVistadas:
-        r, Ciclo2 = buscarSubcicloEuleriano(grafo, x, C)
+        if naoVisitado == -1:   # Todas as arestas foram visitadas
+            break
+
+        # Consulta se há subciclo
+        r, Ciclo2 = buscarSubcicloEuleriano(grafo, naoVisitado, C)
+
         if r is False:
-            return False, None
+            return False, None  # Não há subciclo, grafo não possui ciclo euleriano
 
-        for i, v in enumerate(Ciclo):
-            if v == x:
-                Ciclo.insert(i+1, x)
-                for u in Ciclo2[::-1]:
+        for i, v in enumerate(Ciclo):   # Adiciona subciclo ao ciclo original
+            if v == naoVisitado:
+                for u in Ciclo2[1:]:
                     Ciclo.insert(i+1, u)
+                    i += 1
+                break
 
     return True, Ciclo
 
 
 def Hierholzer(grafo):
-    C = []
+    C = []  # C é uma lista de arestas
 
-    v = grafo.vertices[0]
+    v = grafo.vertices[0]   # Seleciona primeiro vértice
 
-    r, Ciclo = buscarSubcicloEuleriano(grafo, v, C)
+    r, Ciclo = buscarSubcicloEuleriano(grafo, v, C) # Busca ciclo euleriano a partir de v
 
     if r is False:
         return False, None
@@ -65,19 +73,21 @@ def Hierholzer(grafo):
     return True, Ciclo
 
 
+def CicloEuleriano(grafo):
+    flag, c = Hierholzer(grafo)
+
+    # Impressão de resultados
+    if flag:
+        print(1)
+
+        cs = ''
+        for v in c:
+            cs += f"{v.rotulo},"
+        print(cs[:-1])
+    else:
+        print(0)
+
+
 grafo = Grafo()
-grafo.ler("grafo_teste.net")
-
-flag, c = Hierholzer(grafo)
-
-if flag:
-    print(1)
-
-    cs = ''
-    for v in c:
-        cs += f"{v.rotulo} "
-    print(cs)
-else:
-    print(0)
-
-
+grafo.ler("GrafosTeste/ContemCicloEuleriano.net")   # Diretório do arquivo do grafo
+CicloEuleriano(grafo)
